52639bc9d17592e68d3d61fda7bb5c4e4343ed93
choukroun-gabriel <choukroun.gabriel@gmail.com>
2020-11-08 09:28:39 +0200

    Added - scores databases until 2006.

--- scrap_scores.py
+++ scrap_scores.py
@@ -1,6 +1,5 @@
 import os
 from selenium import webdriver, common
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import time
 import csv
@@ -55,14 +54,8 @@ def scores_tournament_data(atp_info, name='tournament_scores', test=False):
     atp_table = []
     # start_time = time.time()
     name, year, url = atp_info[0], atp_info[1], atp_info[2]
-    # Invisible browser
-    options = Options()
-    options.add_argument('--headless')
-    driver = webdriver.Chrome(PATH, options=options)
+    driver = webdriver.Chrome(PATH)
     driver.get(url)
-    table = driver.find_element_by_class_name('day-table')
-    tbody = table.find_elements_by_tag_name('tbody')
-    thead = table.find_elements_by_tag_name('thead')
     # Count the number of KOs
     KO_count = 0
     # Formatting the name of csv
@@ -75,13 +68,16 @@ def scores_tournament_data(atp_info, name='tournament_scores', test=False):
         writer = csv.writer(csv_file)
         # Check if the file is empty. If so, we write the header.
         if not header: writer.writerow(COLUMNS)
-
-        for head, body in zip(thead, tbody):
-            rounds = head.find_element_by_tag_name('th').text
-            tr_l = body.find_elements_by_tag_name('tr')
-            for tr in tr_l:
+        try:
+            table = driver.find_element_by_class_name('day-table')
+            tbody = table.find_elements_by_tag_name('tbody')
+            thead = table.find_elements_by_tag_name('thead')
+            for head, body in zip(thead, tbody):
                 winner = winner_url = loser = loser_url = score = url_detail = url_check = None
-                try:
+                rounds = head.find_element_by_tag_name('th').text
+                tr_l = body.find_elements_by_tag_name('tr')
+                for tr in tr_l:
+
                     winner = tr.find_elements_by_class_name('day-table-name')[0].text
                     winner_url = tr.find_elements_by_class_name('day-table-name')[0] \
                         .find_element_by_tag_name('a').get_attribute('href')
@@ -92,18 +88,19 @@ def scores_tournament_data(atp_info, name='tournament_scores', test=False):
                     url_detail = tr.find_element_by_class_name('day-table-button') \
                         .find_element_by_tag_name('a').get_attribute('href')
                     url_check = 'OK'
-                except common.exceptions.NoSuchElementException:
-                    url_check = 'KO'
-                    KO_count += 1
-                new_row = [name, year, url, rounds, loser, winner, score, url_detail,
-                           winner_url, loser_url, url_check]
-                # We write the new line into the csv
-                writer.writerow(new_row)
+                    new_row = [name, year, url, rounds, loser, winner, score, url_detail,
+                               winner_url, loser_url, url_check]
+                    # We write the new line into the csv
+                    writer.writerow(new_row)
+        except common.exceptions.NoSuchElementException:
+            url_check = 'KO'
+            KO_count += 1
+            writer.writerow([name, year, url, None, None, None, None, None, None, url_check])
 
     driver.close()
     if test:
         print('Test. Exit')
-        print('Time:{}s'.format(time.time() - start_time))
+    #    print('Time:{}s'.format(time.time() - start_time))
         sys.exit(1)
     print("URL - {} - treated with : {} KO".format(url, KO_count))
     return atp_table
@@ -111,8 +108,10 @@ def scores_tournament_data(atp_info, name='tournament_scores', test=False):
 
 def extract_scores(path= PATH_CSV, name='tournament_scores'):
     # Get urls and some other information from tournament csv file
-    atp_infos = get_urls(path)
+    atp_infos = get_urls(path)[900:]
     # Extract data from atp selected urls and write them in a csv
+    # 180 + 80 + 639 + 2757 +
+
     atp_table = []
     for i, atp_info in enumerate(atp_infos):
         atp_table.append(scores_tournament_data(atp_info))
