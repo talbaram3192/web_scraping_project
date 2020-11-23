19f4cc60633665acc79f69f2f27718e2e36b97ec
tal baram <talbaram3192@gmail.com>
2020-11-23 11:28:06 -0600

    updated players page, added sql queries and changed exceptions

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 import requests
-import os
 from selenium import webdriver, common
-from selenium.webdriver.common.keys import Keys
-import time
-import csv
-import sys
 import argparse
-import re
 import players_profile
-from datetime import datetime
 import mysql.connector
 
 CON = mysql.connector.connect(port='3307', user='root', password='Twtcmss2954455'
@@ -57,117 +50,104 @@ def general_tournament_data(url, filter):
 
     for i in tr:  # each 'tr' tag holds the relevant information regarding each tournament in the URL
 
-        try:    #find which type of tournament it is- 250, 500, 1000, grand slam, finals?
+        try:    # find which type of tournament it is- 250, 500, 1000, grand slam, finals?
             td_class = i.find_element_by_class_name('tourney-badge-wrapper')
             tourn_type = td_class.find_element_by_tag_name('img').get_attribute('src')
             new_tourn_type = tourn_type.split('_')[1].split('.')[0]
         except common.exceptions.NoSuchElementException:
             new_tourn_type = 'NA'
-            pass
 
-        if filter == new_tourn_type or filter == 'all':
-            td_content = i.find_element_by_class_name('title-content')  # basic details- name, location and dates
-            name = td_content.find_element_by_class_name('tourney-title').text  # tournament's name
-            location = td_content.find_element_by_class_name('tourney-location').text  # tournament's location
-            dates = td_content.find_element_by_class_name('tourney-dates').text  # tournament's dates
+        # check if tournament exists in DB
 
-            td_draw = i.find_elements_by_class_name('tourney-details')[0]  # number of participants in the draw
-            draw_singles = int(td_draw.find_elements_by_tag_name('span')[0].text) # singles- draw
-            draw_doubles = int(td_draw.find_elements_by_tag_name('span')[1].text)  # doubles- draw
+        td_content = i.find_element_by_class_name('title-content')  # basic details- name, location and dates
+        name = td_content.find_element_by_class_name('tourney-title').text  # tournament's name
+        cursor = CON.cursor()
+        cursor.execute("select * from tournaments where name = %s "
+                       "and year = %s ", [name, year])  # check if tournament exist in DB,
+        check_exist = cursor.fetchall()
+        if len(check_exist) == 0:   # if tournament doesnt exist
+            if filter == new_tourn_type or filter == 'all':
 
-            surface = i.find_elements_by_class_name('tourney-details')[1].text  # surface type
+                location = td_content.find_element_by_class_name('tourney-location').text  # tournament's location
+                dates = td_content.find_element_by_class_name('tourney-dates').text  # tournament's dates
 
-            prize_money = int(i.find_elements_by_class_name('tourney-details')[2].text[1:].replace(',', ''))  # prize money
+                td_draw = i.find_elements_by_class_name('tourney-details')[0]  # number of participants in the draw
+                draw_singles = int(td_draw.find_elements_by_tag_name('span')[0].text) # singles- draw
+                draw_doubles = int(td_draw.find_elements_by_tag_name('span')[1].text)  # doubles- draw
 
-            td_winners = i.find_elements_by_class_name('tourney-details')[3]  # winners
-            winners = td_winners.find_elements_by_class_name('tourney-detail-winner')
-            url_tournament = extract_url_result(i)
+                surface = i.find_elements_by_class_name('tourney-details')[1].text  # surface type
 
-            # get the tournament winners:
-            if len(winners) == 0:
-                winner_singles = None  # if there aren't any winners (tournament didn't happen yet or
-                winners_doubles = None  # got canceled)- don't save any winners
-                winners_team = None
-            elif len(winners) == 1:  # one winner- either singles, doubles or team
-                if winners[0].text[:3] == 'SGL':
-                    winner_singles = winners[0].text[5:]
-                    winners_doubles = None
-                    winners_team = None
-                    try:  # get players links to profiles
-                        winner_profile = winners[0].find_element_by_tag_name('a').get_attribute('href')
+                try:
+                    prize_money = i.find_elements_by_class_name('tourney-details')[2].text[1:].replace(',', '')
+                    if prize_money != '' and prize_money[1].isalpha():
+                        prize_money = int(i.find_elements_by_class_name('tourney-details')[2].text[2:].replace(',', ''))  # prize money
+                    else:
+                        prize_money = int(i.find_elements_by_class_name('tourney-details')[2].text[2:].replace(',', ''))
+                except Exception:
+                    prize_money = None
 
-                        players_profile.get_players_info(winner_profile)
-                    except common.exceptions.NoSuchElementException:
-                        pass   # link doesn't exist
-                    except IndexError:
-                        pass
+                td_winners = i.find_elements_by_class_name('tourney-details')[3]  # winners
+                winners = td_winners.find_elements_by_class_name('tourney-detail-winner')
+                url_tournament = extract_url_result(i)
 
-                elif winners[0].text[:3] == 'DBL':
-                    winner_singles = None
-                    winners_doubles = winners[0].text[5:]
-                    winners_team = None
-
-                    try:     # get players links to profiles
-                        a_tags = winners[0].find_elements_by_tag_name('a')
-                        for a in a_tags:
-                            players_profile.get_players_info(a.get_attribute('href'))
-
-                    except common.exceptions.NoSuchElementException:
-                        pass  # link doesn't exist
-                    except IndexError:
-                        pass
-                else:
-                    winner_singles = None
-                    winners_doubles = None
-                    winners_team = winners[0].text[6:]   # teams dont have profiles so there's no links to add
-                                                         # in this case
-                    # TODO: add team to teams db
-                    # TODO: organize the exeptions normally
-                    # TODO: delete 'None' when not needed..
-                    # TODO: edit tournaments date's
-                    # TODO: add logging
-                    # TODO: add tour finals
-
-            else:  # len=3. two types of winners- singles and doubles (there are no tournaments that include
-                for winner in winners:  # singles, doubles and team winners)
-                    if winner.text[:3] == 'SGL':
-                        winner_singles = winner.text[5:]
-                        try:   # get players links to profiles
-                            winner_profile = winner.find_element_by_tag_name('a').get_attribute('href')
-
+                # get the tournament winners:
+                if len(winners) == 1:  # one winner- either singles, doubles or team
+                    if winners[0].text[:3] == 'SGL':
+                        try:  # get players links to profiles
+                            winner_profile = winners[0].find_element_by_tag_name('a').get_attribute('href')
                             players_profile.get_players_info(winner_profile)
                         except common.exceptions.NoSuchElementException:
-                            pass  # link doesn't exist
+                            pass   # link doesn't exist
                         except IndexError:
                             pass
-                    else:
-                        winners_doubles = winner.text[5:]
-                        try:   # get players links to profiles
-                            a_tags = winner.find_elements_by_tag_name('a')
+                    elif winners[0].text[:3] == 'DBL':
+                        try:     # get players links to profiles
+                            a_tags = winners[0].find_elements_by_tag_name('a')
                             for a in a_tags:
-
                                 players_profile.get_players_info(a.get_attribute('href'))
                         except common.exceptions.NoSuchElementException:
                             pass  # link doesn't exist
                         except IndexError:
                             pass
+                    else:
+                        try:
+                            winners_team = winners[0].text[6:]   # teams dont have profiles so there's no links to add
+                        except common.exceptions.NoSuchElementException:
+                            pass  # link doesn't exist
+                        except IndexError:
+                            pass
+                                                             # in this case
+                        # TODO: add team to teams db
+                        # TODO: organize the exeptions normally
+                        # TODO: delete 'None' when not needed..
+                        # TODO: edit tournaments date's
+                        # TODO: add logging
+                        # TODO: add tour finals
 
-                winners_team = None
+                else:  # len=3. two types of winners- singles and doubles (there are no tournaments that include
+                    for winner in winners:  # singles, doubles and team winners)
+                        if winner.text[:3] == 'SGL':
+                            try:   # get players links to profiles
+                                winner_profile = winner.find_element_by_tag_name('a').get_attribute('href')
+                                players_profile.get_players_info(winner_profile)
+                            except common.exceptions.NoSuchElementException:
+                                pass  # link doesn't exist
+                            except IndexError:
+                                pass
+                        else:
+                            try:   # get players links to profiles
+                                a_tags = winner.find_elements_by_tag_name('a')
+                                for a in a_tags:
+                                    players_profile.get_players_info(a.get_attribute('href'))
+                            except common.exceptions.NoSuchElementException:
+                                pass  # link doesn't exist
+                            except IndexError:
+                                pass
 
-
-            # print(year,new_tourn_type,name,location,dates,draw_singles,draw_doubles,surface
-            #       ,prize_money,winner_singles,winners_doubles,winners_team)
-
-            cursor = CON.cursor()
-            cursor.execute("select name from tournaments where name = %s ", [name]) # check if tournament exist in DB,
-            tour_name = cursor.fetchall()    # tournaments have unique names.
-            if len(tour_name) == 0:
                 cursor.execute(''' insert into tournaments (year,type,name,location,date,SGL_draw,
-                DBL_draw, surface, prize_money) values(%s,%s,%s,%s,%s,%s,%s,%s,%s) ''', [year, new_tourn_type,
-                                                                                          name, location, dates,
-                                                                                          draw_singles, draw_doubles,
-                                                                                          surface, prize_money])
-
+                    DBL_draw, surface, prize_money) values(%s,%s,%s,%s,%s,%s,%s,%s,%s) ''', [year, new_tourn_type,
+                                                                                              name, location, dates,
+                                                                                              draw_singles, draw_doubles,                                                                        surface, prize_money])
                 CON.commit()
 
     driver.close()
--- players_profile.py
+++ players_profile.py
@@ -6,50 +6,77 @@ from selenium import webdriver, common
 def get_players_info(player_url):
     """ Get players information from their profiles """
 
-    # TODO: add to DB- make sure data types are correct and change accordingly (prize money- int, weight,height- float)
-
     driver = webdriver.Chrome(main.PATH)
     driver.get(player_url)
-    try:
-        first = driver.find_element_by_class_name('first-name').text
-        print(first)
 
-        last = driver.find_element_by_class_name('last-name').text
-        print(last)
+    # check if player exists in the DB
+    first = driver.find_element_by_class_name('first-name').text
+    print(first)
 
-        ranking_sgl = driver.find_elements_by_class_name('stat-value')[0].get_attribute('data-singles')  # current ranking- singles
-        print(ranking_sgl)
-        ranking_dbl = driver.find_elements_by_class_name('stat-value')[0].get_attribute('data-doubles')  # current ranking- doubles
-        print(ranking_dbl)
+    last = driver.find_element_by_class_name('last-name').text
+    print(last)
 
-        career_high_sgl = driver.find_elements_by_class_name('stat-value')[5].get_attribute('data-singles') # career high ranking- singles
+    cursor = main.CON.cursor()
+    cursor.execute("select * from players where first_name = %s "
+                   "and last_name = %s ", [first, last])
+    check_exist = cursor.fetchall()
+    if len(check_exist) == 0 and first != 'NA':  # if player doesnt exist in DB but has details in the website
+
+        try:
+            ranking_sgl = int(driver.find_elements_by_class_name('stat-value')[0].get_attribute('data-singles'))  # current ranking- singles
+            print(ranking_sgl)
+            ranking_dbl = int(driver.find_elements_by_class_name('stat-value')[0].get_attribute('data-doubles'))  # current ranking- doubles
+            print(ranking_dbl)
+        except Exception:
+            ranking_sgl = None
+            ranking_dbl = None
+
+        career_high_sgl = int(driver.find_elements_by_class_name('stat-value')[5].get_attribute('data-singles')) # career high ranking- singles
         print(career_high_sgl)
-        career_high_dbl = driver.find_elements_by_class_name('stat-value')[5].get_attribute('data-doubles')  # career high ranking- doubles
+        career_high_dbl = int(driver.find_elements_by_class_name('stat-value')[5].get_attribute('data-doubles'))  # career high ranking- doubles
         print(career_high_dbl)
 
-        country = driver.find_element_by_class_name('player-flag-code').text # country
-        print(country)
+        try:
+            country = driver.find_element_by_class_name('player-flag-code').text # country
+            print(country)
+        except Exception:
+            country = None
 
-        date_birth = driver.find_element_by_class_name('table-birthday').text.strip('()')  # date of birth
-        print(date_birth)
+        try:
+            date_birth = driver.find_element_by_class_name('table-birthday').text.strip('()')  # date of birth
+            print(date_birth)
+        except Exception:
+            date_birth = None
+        try:
+            turned_pro = int(driver.find_elements_by_class_name('table-big-value')[1].text) # turned pro
+            print(turned_pro)
+        except Exception:
+            turned_pro = None
+        try:
+            weight = float(driver.find_element_by_class_name('table-weight-lbs').text)  # weight
+            print(weight)
 
-        turned_pro = driver.find_elements_by_class_name('table-big-value')[1].text # turned pro
-        print(turned_pro)
+            height = float(driver.find_element_by_class_name('table-height-cm-wrapper').text[1:4])  # height
+            print(height)
+        except Exception:
+            weight = None
+            height = None
+        try:
+            total_prize_money = int(driver.find_elements_by_class_name('stat-value')[8].text.split()[0][1:].replace(',', '')) # total prize money
+            print(total_prize_money)
+        except Exception:
+            total_prize_money = None
 
-        weight = driver.find_element_by_class_name('table-weight-lbs').text  # weight
-        print(weight)
+        cursor.execute(''' insert into players (first_name,last_name,ranking_DBL,ranking_SGL,
+        career_high_DBL,career_high_SGL,turned_pro, weight,height, total_prize_money, country, birth)
+        values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) ''',
+                       [first, last,
+                        ranking_dbl, ranking_sgl, career_high_dbl,
+                        career_high_sgl, turned_pro, weight, height,
+                        total_prize_money, country, date_birth])
 
-        height = driver.find_element_by_class_name('table-height-ft').text  # height
-        print(height)
+        main.CON.commit()
 
-        total_prize_money = driver.find_elements_by_class_name('stat-value')[8].text.split()[0] # total prize money
-        print(total_prize_money)
-
-    except IndexError:
-        pass
-    except common.exceptions.NoSuchElementException:
-        pass
-    except:
-        pass
-
-    driver.close()
+        driver.close()
+    else:
+        driver.close()
